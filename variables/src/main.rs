c125ee19d6cc87697adc40ecb513e65bfb79c8a8
Redstone Zhao <zhaoxiongfei@admaster.com.cn>
2019-06-20 15:40:16 +0800

    新增变量，数据类型的学习代码

---
+++ variables/src/main.rs
@@ -0,0 +1,35 @@
+const MAX_POINTS: u32 = 100_000_000;
+const MAX_POINTS_2: u32 = 1000 * 1000;
+
+fn main() {
+    let x: f64 = 0.0;
+    println!("This value of x is: {}", x);
+    let x: f64 = x - 1.0;
+    let y = x / 2.5;
+    println!("This value of x is: {}", x);
+    println!("This value of y is: {}", y);
+    println!("MAX_POINTS value is: {}", MAX_POINTS);
+    println!("MAX_POINTS_2 value is: {}", MAX_POINTS_2);
+
+    let tup: (i32, f64, u8) = (500, 2.3, 31);
+    let (x, y, z) = tup;
+    println!("x = {}, y = {}, z = {}", x, y, z);
+    println!("tup.0 = {}, tup.1 = {}, tup.2 = {}", tup.0, tup.1, tup.2);
+
+    // array
+    let array = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    println!("Array first item is: {}", array[0]);
+}
